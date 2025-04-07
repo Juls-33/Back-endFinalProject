bc23050f4efef761747a2a49b26c688d9a91e7eb
Juls-33 <juliusaustin.santos@gmail.com>
2025-04-07 23:34:52 +0800

    test

---
+++ Le SanShoes/users/sampleIndex.php
@@ -0,0 +1,19 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>UserAccounts - Home</title>
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" />
+        <!-- Custome styles -->
+        <link rel="stylesheet" href="static/css/style.css">
+        <!-- JQuery -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <!-- Bootstrap JS -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+        <div><a href="sampleLogin.php"></a>Login</div>
+    </body>
+</html>
