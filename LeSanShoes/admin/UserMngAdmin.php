d6e22af10e18555c07abea95dc7b1f967e95a673
Juls-33 <juliusaustin.santos@gmail.com>
2025-04-24 16:53:00 +0800

    editDB

--- LeSanShoes/admin/UserMngAdmin.php
+++ LeSanShoes/admin/UserMngAdmin.php
@@ -298,9 +298,10 @@
             </div>
             <!-- end of delete modal -->
             
+            <!-- Navbar table -->
             <nav>
                 <div class="nav nav-tabs" id="nav-tab" role="tablist">
-                    <button class="nav-link active" id="nav-h  ome-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">Admin Accounts</button>
+                    <button class="nav-link active" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">Admin Accounts</button>
                     <button class="nav-link" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">User Accounts</button>
                 </div>
             </nav>
@@ -324,7 +325,7 @@
                     </div>
                 </div>
                 <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
-                    <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
+                    <!-- <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab"> -->
                         <div class="container">
                             <table id="usersTable" class="display" style="width:100%">
                                 <thead>
@@ -342,7 +343,7 @@
                                 </thead>
                             </table>
                         </div>
-                    </div>
+                    <!-- </div> -->
                 </div>
             </div>
         </div>
