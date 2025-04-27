f3a1e7eec8f6745891c2eb03526da846ca783453
Juls-33 <juliusaustin.santos@gmail.com>
2025-04-27 16:14:57 +0800

    eyyy

--- LeSanShoes/includes/logic/manageDBMiddleware.js
+++ LeSanShoes/includes/logic/manageDBMiddleware.js
@@ -1,3 +1,4 @@
+loadTables();
 function checkOneField(formData){
     if (!formData.formField) {
         Swal.fire({
@@ -195,6 +196,7 @@ function sendViaAJAX(formData){
                         no-repeat
                     `
                     });
+                    loadTables();
             }
             else{
                 Swal.fire({
@@ -225,6 +227,80 @@ function sendViaAJAX(formData){
             });
         }
     });
+}
+
+function loadTables(){
+    $(document).ready(function() {
+        $.ajax({
+            url: '../includes/logic/getManageDB.php',
+            method: 'POST',
+            dataType: 'json',
+            success: function(response) {
+                $('#brandTable').DataTable().clear().destroy();
+                $('#categoryTable').DataTable().clear().destroy();
+                $('#genderTable').DataTable().clear().destroy();
+                $('#statusTable').DataTable().clear().destroy();
+                $('#rolesTable').DataTable().clear().destroy();
+                // Brand Table
+                $('#brandTable').DataTable({
+                    destroy: true,
+                    data: response.brands,
+                    columns: [
+                        { data: 'brand_id' },
+                        { data: 'brand_name' },
+                        { data: 'date_created' },
+                        { data: 'date_updated' }
+                    ]
+                });
+    
+                // Category Table
+                $('#categoryTable').DataTable({
+                    destroy: true,
+                    data: response.categories,
+                    columns: [
+                        { data: 'category_id' },
+                        { data: 'category_name' },
+                        { data: 'date_created' },
+                        { data: 'date_updated' }
+                    ]
+                });
+                // Shoes gender Table
+                $('#genderTable').DataTable({
+                    destroy: true,
+                    data: response.shoes_gender,
+                    columns: [
+                        { data: 'shoes_gender_id' },
+                        { data: 'shoes_gender_name' },
+                        { data: 'date_created' },
+                        { data: 'date_updated' }
+                    ]
+                });
+                // Status Table
+                $('#statusTable').DataTable({
+                    destroy: true,
+                    data: response.statusData,
+                    columns: [
+                        { data: 'status_id' },
+                        { data: 'status_name' },
+                        { data: 'date_created' },
+                        { data: 'date_updated' }
+                    ]
+                });
+                $('#rolesTable').DataTable({
+                    destroy: true,
+                    data: response.roles,
+                    columns: [
+                        { data: 'roles_id' },
+                        { data: 'roles_name' },
+                        { data: 'roles_desc' },
+                        { data: 'date_created' },
+                        { data: 'date_updated' }
+                    ]
+                });
+            },
+        });
+    });
+    
 }
 
 
