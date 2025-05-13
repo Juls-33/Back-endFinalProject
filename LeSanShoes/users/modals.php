fa0742349a927c2fcbff2a693808c436db22fff3
andrei-vj22 <andreipogivillajuan@gmail.com>
2025-05-13 23:32:59 +0800

    Update modals.php

--- LeSanShoes/users/modals.php
+++ LeSanShoes/users/modals.php
@@ -60,3 +60,89 @@
     </div>
   </div>
 </div>
+
+
+<!-- edit users modal -->
+ <button type="button" class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#editUserModal">EDIT</button>
+<!-- User Edit Modal HTML -->
+<div id="editUserModal" class="modal fade" tabindex="-1" aria-labelledby="editUserModalLabel">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-btn btn-primary" id="editUserModalLabel">Edit User Details</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close" id="editUserModalCloseBtn"></button>
+            </div>
+
+            <div class="modal-body">    
+                <!-- Tab Navigation -->
+                <ul class="nav nav-tabs" id="editUserTabs" role="tablist">
+                    <li class="nav-item" role="presentation">
+                        <a class="nav-link active" id="edit-username-tab" data-bs-toggle="tab" href="#edit-username" role="tab" aria-controls="edit-username" aria-selected="true">Username</a>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <a class="nav-link" id="edit-email-tab" data-bs-toggle="tab" href="#edit-email" role="tab" aria-controls="edit-email" aria-selected="false">Email</a>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <a class="nav-link" id="edit-address-tab" data-bs-toggle="tab" href="#edit-address" role="tab" aria-controls="edit-address" aria-selected="false">Address</a>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <a class="nav-link" id="edit-password-tab" data-bs-toggle="tab" href="#edit-contact" role="tab" aria-controls="edit-contact" aria-selected="false">Contact</a>
+                    </li>
+                </ul>
+
+                <!-- Tab Content -->
+                <div class="tab-content mt-3">
+                    <!-- Username Tab -->
+                    <div class="tab-pane fade show active" id="edit-username" role="tabpanel" aria-labelledby="edit-username-tab">
+                        <div class="form-group mb-3">
+                          <form id="nameForm">
+                            <label for="username" class="form-label">First Name</label>
+                            <input type="text" id="fname" name="fname" class="form-control" placeholder="Enter new first name" maxlength="100" required>
+                            <label for="username" class="form-label">Last Name</label>
+                            <input type="text" id="lname" name="lname" class="form-control" placeholder="Enter new last name" maxlength="100" required>
+                            <button type="button" class="btn btn-primary" onclick="updateName()">Save Details</button>
+                          </form>
+                        </div>
+                    </div>
+
+                    <!-- Email Tab -->
+                    <div class="tab-pane fade" id="edit-email" role="tabpanel" aria-labelledby="edit-email-tab">
+                        <div class="form-group mb-3">
+                            <form id="emailForm">
+                              <label for="username" class="form-label">Email</label>
+                              <input type="EMAIL" id="email" name="email" class="form-control" placeholder="Enter new email" maxlength="100" required>
+                              <button type="button" class="btn btn-primary" onclick="updateEmail()">Save Details</button>
+                            </form>
+                        </div>
+                    </div>
+
+                    <!-- Address Tab -->
+                    <div class="tab-pane fade" id="edit-address" role="tabpanel" aria-labelledby="edit-address-tab">
+                        <div class="form-group mb-3">
+                            <form id="addressForm">
+                              <label for="username" class="form-label">Address</label>
+                              <input type="text" id="address" name="address" class="form-control" placeholder="Enter new address" maxlength="250" required>
+                              <button type="button" class="btn btn-primary" onclick="updateAddress()">Save Details</button>
+                            </form>
+                        </div>
+                    </div>
+
+                    <!-- Contact Tab -->
+                    <div class="tab-pane fade" id="edit-contact" role="tabpanel" aria-labelledby="edit-contact-tab">
+                        <div class="form-group mb-3">
+                            <form id="contactForm">
+                              <label for="username" class="form-label">Contact Number</label>
+                              <input type="text" id="contact" name="contact" class="form-control" placeholder="Enter new contact number" maxlength="11" required>
+                              <button type="button" class="btn btn-primary" onclick="updateContact()">Save Details</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" id="editUserModalCloseFooterBtn">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
