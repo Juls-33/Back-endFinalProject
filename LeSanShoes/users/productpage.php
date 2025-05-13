81ebd8f1432f93dc49b01995aa8b535d23f48ba5
Juls-33 <juliusaustin.santos@gmail.com>
2025-05-13 15:02:18 +0800

    view orders sa lahat

--- LeSanShoes/users/productpage.php
+++ LeSanShoes/users/productpage.php
@@ -146,64 +146,9 @@
 <!-- end of size chart modal -->
 
 <!-- MODAL CART -->
-<div class="modal fade" id="cartModal" tabindex="-1" aria-labelledby="cartModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-lg">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="cartModalLabel">Your Cart</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body" id="cartModalBody">
-                <div id="cartItems">
-                    <!-- <div class="card mb-3" style="max-width: fit-content;">
-                        <div class="row rowcart g-0">
-                            <div class="col-md-4" style="padding-left: 10px;">
-                                <img src="user-homepage/images/pdimg/kai1.jpg" class="img-fluid rounded-start" alt="Nike V2K Run" style="object-fit: contain;">
-                            </div>
-                            <div class="col-md-8">
-                                <div class="card-body">
-                                    <h5 class="card-title">Kaiju 1</h5>
-                                    <p class="card-text">Men's Shoes</p>
-                                    <p class="card-text">
-                                        <small class="text-muted">Vintage Green/Mineral Spruce/Wolf Grey/Vintage Green</small>
-                                    </p>
-                                    <div class="dropdown mb-2">
-                                        <button class="btn btn-secondary dropdown-toggle" type="button" id="sizeDropdown" data-bs-toggle="dropdown" aria-expanded="false">
-                                            Size: US 10
-                                        </button>
-                                        <ul class="dropdown-menu" aria-labelledby="sizeDropdown">
-                                            <li><a class="dropdown-item" href="#">US 9.5</a></li>
-                                            <li><a class="dropdown-item" href="#">US 9</a></li>
-                                            <li><a class="dropdown-item" href="#">US 8.5</a></li>
-                                            <li><a class="dropdown-item" href="#">US 8</a></li>
-                                        </ul>
-                                    </div>
-                                    <p class="card-text"><strong>₱6,895.00</strong></p>
-                                    <div class="d-flex align-items-center">
-                                        <button class="btn btn-outline-secondary mbtn me-2">-</button>
-                                        <span>1</span>
-                                        <button class="btn btn-outline-secondary mbtn ms-2">+</button>
-                                        <button class="btn btn-outline-danger mbtn ms-2">
-                                            <i class="material-icons">delete</i>
-                                        </button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div> -->
-                    <!-- Additional items can be added here -->
-                </div>
-            </div>
-            <div id="cartFooter" class="modal-footer d-flex justify-content-between" style="display: none;">
-                <hr>
-                <p><strong>Subtotal: <span id="cartSubtotal">₱0.00</span></strong></p>
-                <button id="checkoutBtn" class="btn btn-danger">Checkout</button>
-            </div>
-        </div>
-    </div>
-</div>
-<!-- end of modal cart -->
 
+<?php include('modals.php'); ?>
+<!-- end of modal cart -->
 <!-- Footer -->
 <footer class="text-white text-center text-lg-start w-100" style="width: 100%; background-color: #B51E1E;">
     <div class="p-4" style="max-width: 100%;">
@@ -286,5 +231,6 @@ function updateCartFooter(isEmpty) {
 
 </script>
 <script src="ppageMiddleware.js"></script>
+<script src="orderDetailsMiddleware.js"></script>
 </body>
 </html>
